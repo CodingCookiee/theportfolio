89cdca93d80b1b5fbc459dbd74225ecd5d77d3fd
CodingCookiee <rzqutubawan@gmail.com>
2024-12-04 11:11:43 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
 function App() {
   return (
     <div className="App text-5xl text-center font-bold">
       <h1>Vite + React</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
