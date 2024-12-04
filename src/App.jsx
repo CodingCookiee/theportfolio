6fa462ff338b2956d74813ca67f516ab9bc52c97
CodingCookiee <rzqutubawan@gmail.com>
2024-12-04 18:40:56 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-    <h1>Hello, World!</h1>
+    <div className=" text-5xl font-bold text-center">
+      <h1>Hello, World!</h1>
     </div>
   );
-}
+};
 
 export default App;
- 
