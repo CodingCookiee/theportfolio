9cd50c4a8abbdb4bd683eb01fa0e7c57771fc53c
CodingCookiee <rzqutubawan@gmail.com>
2024-12-05 01:11:32 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import Experience from "./sections/Experience";
@@ -8,22 +9,43 @@ import Footer from "./sections/Footer";
 import { ThemeProvider } from "./utils/ThemeContext";
 
 const App = () => {
-  return (
-    <ThemeProvider>
-      <div className="App min-h-screen flex flex-col dark:bg-dark-primary bg-light-primary transition-colors duration-300">
-        <Navbar />
-        <main className="flex-1 max-w-7xl w-full mx-auto">
-          <section className="h-[1000px]"></section>
+  const [loading, setLoading] = useState(true);
 
-          <Hero />
-          <Experience />
-          <Projects />
-          <Skills />
-          <Contact />
-        </main>
-        <Footer />
-      </div>
-    </ThemeProvider>
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds loading time
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      ) : (
+        <ThemeProvider>
+          <div className="App min-h-screen flex flex-col dark:bg-dark-primary bg-light-primary transition-colors duration-300">
+            <Navbar />
+            <main className="flex-1 max-w-7xl w-full mx-auto">
+              <Hero />
+              <Experience />
+              <Projects />
+              <Skills />
+              <Contact />
+            </main>
+            <Footer />
+          </div>
+        </ThemeProvider>
+      )}
+    </>
   );
 };
 
